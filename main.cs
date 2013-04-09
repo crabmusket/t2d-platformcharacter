2c771561e0bb881dd6a16481062edb10d58ed065
Daniel Buckmaster <dan.buckmaster@gmail.com>
2013-04-10 00:40:15 +1000

    Added basics, including control.

--- main.cs
+++ main.cs
@@ -1,5 +1,30 @@
-function PlatformCharacter::create() {
+function PlatformCharacter::create(%this) {
+   if(!isObject(PlatformControls)) {
+      new ActionMap(PlatformControls);
+      PlatformControls.bindObj(keyboard, a, "left", %this);
+      PlatformControls.bindObj(keyboard, d, "right", %this);
+      PlatformControls.push();
+   }
 }
 
-function PlatformCharacter::destroy() {
+function PlatformCharacter::destroy(%this) {
+   if(isObject(PlatformControls)) {
+      PlatformControls.pop();
+      PlatformControls.delete();
+   }
 }
+
+function PlatformCharacter::left(%this, %val) {
+   echo("left" SPC %val);
+}
+
+function PlatformCharacter::right(%this, %val) {
+   echo("right" SPC %val);
+}
+
+function PlatformCharacter::spawn(%input) {
+   %p = new Sprite();
+   %p.setSize("1 2");
+   return %p;
+}
+
