1336cc7003cd8ef1c7f316d6a73abb91dc944f12
Daniel Buckmaster <dan.buckmaster@gmail.com>
2013-04-10 12:49:02 +1000

    Added air control/ground detection.

--- main.cs
+++ main.cs
@@ -18,6 +18,7 @@ function PlatformCharacter::destroy(%this) {
 $PlatformCharacter::DefaultMoveSpeed = 50;
 $PlatformCharacter::DefaultJumpSpeed = 5;
 $PlatformCharacter::DefaultFriction = 1.1;
+$PlatformCharacter::DefaultAirControl = 0.3;
 
 function PlatformCharacter::spawn(%input) {
    // Appearance
@@ -29,6 +30,7 @@ function PlatformCharacter::spawn(%input) {
    %p.FixedAngle = true;
    %p.setDefaultFriction($PlatformCharacter::DefaultFriction);
    %p.groundCollisionShape = %p.createPolygonBoxCollisionShape(1, 2);
+   %p.setGatherContacts(true);
 
    // Movement
    %p.moveX = 0;
@@ -41,6 +43,7 @@ function PlatformCharacter::spawn(%input) {
    // Character properties
    %p.moveSpeed = $PlatformCharacter::DefaultMoveSpeed;
    %p.jumpSpeed = $PlatformCharacter::DefaultJumpSpeed;
+   %p.airControl = $PlatformCharacter::DefaultAirControl;
 
    // Control
    switch$(%input) {
@@ -78,7 +81,22 @@ function PlatformCharacterSprite::onUpdate(%this) {
    // Update movement force
    if(%this.moveX != 0) {
       %velX = %this.getLinearVelocityX();
-      %force = %this.moveX / (mAbs(%velX) + 1) SPC 0;
-      %this.applyForce(%force, %this.getPosition());
+      %force = %this.moveX / (mAbs(%velX) + 1);
+      if(!%this.isTouchingGround()) {
+         echo("not on ground" SPC %this.airControl);
+         %force *= %this.airControl;
+      }
+      %this.applyForce(%force SPC 0, %this.getPosition());
    }
+}
+
+function PlatformCharacterSprite::isTouchingGround(%this) {
+   %i = 0;
+   while(%i < %this.getContactCount()) {
+      if(getWord(%this.getContact(%i), 1) == %this.groundCollisionShape) {
+         return true;
+      }
+      %i++;
+   }
+   return false;
 }
